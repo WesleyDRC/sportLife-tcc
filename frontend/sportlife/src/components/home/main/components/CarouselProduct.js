e50f5ffaebdfcf22a44e36f433a88e19a01fdcd8
pablobidiaki <pablobidiaki@hotmail.com>
2023-03-05 16:32:37 -0300

    carrosel de itens

--- frontend/sportlife/src/components/home/main/components/CarouselProduct.js
+++ frontend/sportlife/src/components/home/main/components/CarouselProduct.js
@@ -1,3 +1,4 @@
+import {useRef} from 'react'
 import styles from './CarouselProduct.module.css'
 
 import {FaArrowLeft} from "react-icons/fa";
@@ -6,19 +7,37 @@ import {FaArrowRight} from "react-icons/fa";
 import Product from './Product';
 
 export default function CarouselProduct(props){
+
+	const carousel = useRef(null)
+
+	const handleLeftClick = (e) => {
+		e.preventDefault()
+		carousel.current.scrollLeft -= carousel.current.offsetWidth
+	}
+
+	const handleRightClick = (e) => {
+		e.preventDefault()
+		carousel.current.scrollLeft += carousel.current.offsetWidth
+	}
+
 	return(
 		<div className={styles.container}>
 			<p>Mais Vistos</p>
 			<div className={styles.carousel}>
-				<FaArrowLeft className={styles.arrow} />
-				<div className={styles.promotions}>
+				<FaArrowLeft className={styles.arrow} onClick={handleLeftClick} />
+				<div className={styles.promotions} ref={carousel}>
+						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
+						<Product src={'https://i.imgur.com/sPqRXIE.jpg'} name={'Camisa preta'}  />
+						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
+						<Product src={'https://i.imgur.com/sPqRXIE.jpg'} name={'Camisa preta'}  />
 						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
 						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
+						<Product src={'https://i.imgur.com/sPqRXIE.jpg'} name={'Camisa preta'}  />
 						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
-						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
+						<Product src={'https://i.imgur.com/sPqRXIE.jpg'} name={'Camisa preta'}  />
 						<Product src={'https://i.imgur.com/LTnbPIK.jpg'} name={'Camisa preta'}  />
 				</div>
-				<FaArrowRight className={styles.arrow} />
+				<FaArrowRight className={styles.arrow} onClick={handleRightClick} />
 			</div>
 
 		</div>
