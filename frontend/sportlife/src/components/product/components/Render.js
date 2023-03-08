33f6728ff2f351523263b7035a915c98fc62c793
pablobidiaki <pablobidiaki@hotmail.com>
2023-03-08 15:39:25 -0300

    feat:"Product Page Details"

--- frontend/sportlife/src/components/product/components/Render.js
+++ frontend/sportlife/src/components/product/components/Render.js
@@ -1,18 +1,43 @@
+import { useState } from 'react'
 import styles from './Render.module.css'
 import AdditionalInfos from './AdditionalInfos'
-import Carousel from '../../home/main/components/Product'
+import Assessements from './Assessments'
 import CarouselProduct from "../../home/main/components/CarouselProduct"
 
 export default function Render(){
-	return(
-		<div className={styles.container}>
-			<hr className={styles.line} />
-			<div className={styles.options}>
-				<p className={`${styles.addInfos} ${styles.borderTop}`}>Informações Adicionais</p>
-				<p className={styles.assessements}>Avaliações</p>
+	const [page, setPage] = useState(0)
+	const ass = document.getElementById('ass')
+	const info = document.getElementById('info')
+	function AssView(){
+			setPage(1)
+	}
+	function InfosView(){
+		setPage(0)
+	}
+
+	if(page == 0){
+		return(
+			<div className={styles.container}>
+				<hr className={styles.line} />
+				<div className={styles.options}>
+					<p className={`${styles.addInfos} ${styles.borderTop}`}>Informações Adicionais</p>
+					<p className={styles.assessements} onClick={AssView} id='ass'>Avaliações</p>
+				</div>
+				<AdditionalInfos />
+				<CarouselProduct titleCarousel="ITENS RELACIONADOS" />
 			</div>
-			<AdditionalInfos />
-			<CarouselProduct titleCarousel="ITENS RELACIONADOS"  />
-		</div>
-	)
+		)
+	}else{
+		return(
+			<div className={styles.container}>
+				<hr className={styles.line} />
+				<div className={styles.options}>
+					<p className={styles.addInfos} onClick={InfosView} id='info'>Informações Adicionais</p>
+					<p className={`${styles.assessements} ${styles.borderTop}`} >Avaliações</p>
+				</div>
+				<Assessements />
+				<CarouselProduct titleCarousel="ITENS RELACIONADOS" />
+			</div>
+		)
+	}
 }
