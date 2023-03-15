38d1918dd69b874b81d02c6db204eb7272097ff1
pablobidiaki <pablobidiaki@hotmail.com>
2023-03-15 16:45:49 -0300

    feat:"create card in category page"

--- frontend/sportlife/src/components/category/components/Card.js
+++ frontend/sportlife/src/components/category/components/Card.js
@@ -1,12 +1,27 @@
 import styles from './Card.module.css'
+import {useState} from 'react'
 
 export default function Card(props){
+
+	const [buttonView, setButtonView] = useState(false)
+
+	function buttonVisible(){
+		setButtonView(true)
+	}
+
+	function buttonNotVisible(){
+		setButtonView(false)
+	}
+
 	return(
-		<div className={styles.container}>
-			<div>
+		<div  className={styles.container}>
+			<div onMouseOver={buttonVisible} onMouseOut={buttonNotVisible} className={styles.card}>
 				<img src={props.link} alt='Foto do produto' />
-				<p>{props.name}</p>
-				<p>{props.price} </p>
+				<p className={styles.name}>{props.name}</p>
+				<p className={styles.price}>{props.price} </p>
+				<div className={styles.positionButton}>
+					<button className={buttonView ? styles.buy : styles.buyNone}>Adicionar ao carrinho</button>
+				</div>
 			</div>
 		</div>
 	)
