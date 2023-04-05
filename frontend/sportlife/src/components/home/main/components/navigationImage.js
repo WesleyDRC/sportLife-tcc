58048c7835c912eb56f27736ae261e25304b2864
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-05 15:33:19 -0300

    refactor:  changing navigation images by sports and fixing details

--- frontend/sportlife/src/components/home/main/components/navigationImage.js
+++ frontend/sportlife/src/components/home/main/components/navigationImage.js
@@ -5,10 +5,10 @@ export default function navigationImage(){
 		<div className={styles.container}>
 			<p className={styles.title}>Navegue por esportes !</p>
 			<div className={styles.photos}>
-				<img className={styles.img1} src='https://i.imgur.com/YGIHZoO.png' alt='soccer'/>
-				<img className={styles.img2} src=' https://i.imgur.com/JBwm5ip.png' alt='basketball'/>
-				<img className={styles.img3} src='https://i.imgur.com/BD1kIzZ.png' alt='running'/>
-				<img className={styles.img4} src=' https://i.imgur.com/ggRkuDw.png' alt='volleyball'/>
+				<img className={styles.img1} src='https://i.imgur.com/lU9oI6w.png' alt='soccer'/>
+				<img className={styles.img2} src='https://i.imgur.com/OZbIsnt.png' alt='basketball'/>
+				<img className={styles.img3} src='https://i.imgur.com/gaPtsCX.png' alt='running'/>
+				<img className={styles.img4} src='https://i.imgur.com/CV0Xe9h.png' alt='volleyball'/>
 			</div>
 		</div>
 	)
