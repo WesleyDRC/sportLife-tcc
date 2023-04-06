7b6e3a98ac3d3a07fce429edc603ee4c55830c04
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-06 12:48:16 -0300

    feat: concepts of responsiveness in navigation by brands

--- frontend/sportlife/src/components/home/main/components/Carousel.js
+++ frontend/sportlife/src/components/home/main/components/Carousel.js
@@ -1,3 +1,5 @@
+import './Carousel.module.css'
+
 export default function Carousel(){
 
 	return(
--- frontend/sportlife/src/components/home/main/components/NavigationBrand.js
+++ frontend/sportlife/src/components/home/main/components/NavigationBrand.js
@@ -1,6 +1,22 @@
 import styles from './NavigationBrand.module.css'
 
+import { useRef } from 'react';
+
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
 export default function NavigationBrand(){
+	const Carousel = useRef(null);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    Carousel.current.scrollLeft -= Carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    Carousel.current.scrollLeft += Carousel.current.offsetWidth;
+  };
 	return(
 		<div className={styles.container}>
 			<p className={styles.title}>Navegue por marcas</p>
@@ -13,6 +29,27 @@ export default function NavigationBrand(){
 				<img className={styles.logo} src='https://i.imgur.com/dCoxQpf.png' alt='Logo Wilson'/>
 				<img className={styles.logo} src='https://i.imgur.com/OCqxTH0.png' alt='Logo Adidas'/>
 			</div>
+
+			<div className={styles.carousel}>
+        <IoIosArrowBack
+          className={styles.arrowLeft}
+          onClick={handleLeftClick}
+        />
+        <div className={styles.logos} ref={Carousel}>
+					<img className={styles.logo} src='https://i.imgur.com/LkO860L.png' alt='Logo Puma'/>
+					<img className={styles.logo} src='https://i.imgur.com/wAYQLwf.png' alt='Logo Fila'/>
+					<img className={styles.logo} src='https://i.imgur.com/Z48eiIA.png' alt='Logo Mizuno'/>
+					<img className={styles.logo} src='https://i.imgur.com/jQogHUN.png' alt='Logo Nike'/>
+					<img className={styles.logo} src='https://i.imgur.com/928xw6j.png' alt='Logo Asics'/>
+					<img className={styles.logo} src='https://i.imgur.com/dCoxQpf.png' alt='Logo Wilson'/>
+					<img className={`${styles.logo} ${styles.lastLogo}`} src='https://i.imgur.com/OCqxTH0.png' alt='Logo Adidas'/>
+        </div>
+        <IoIosArrowForward
+          className={styles.arrowRight}
+          onClick={handleRightClick}
+        />
+			</div>
+
 			<div className={styles.containerCards}>
 				<img src='https://i.imgur.com/0Lq4onL.png' alt='Card Puma'/>
 				<img src='https://i.imgur.com/v5BiuP5.png' alt='Card Nike'/>
