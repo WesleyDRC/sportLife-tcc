3ad89e77c0d1ea918e14896e184d58b78eb23b43
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-18 14:46:40 -0300

    feat: creating the purchase summary component

--- frontend/sportlife/src/components/confirmBuy/components/Summary.js
+++ frontend/sportlife/src/components/confirmBuy/components/Summary.js
@@ -4,20 +4,20 @@ export default function Summary(){
 	return(
 		<div className={styles.container}>
 			<h2>resumo da compra</h2>
-			<div>
+			<div className={styles.subTotal}>
 				<p>sub-total</p>
 				<p>R$279,99</p>
 			</div>
-			<div>
+			<div className={styles.delivery}>
 				<p>entrega</p>
 				<p>R$0,00</p>
 			</div>
-			<div>
+			<div className={styles.total}>
 				<p>total</p>
-				<p>R$279,00</p>
+				<p className={styles.priceTotal}>R$279,00</p>
 			</div>
-			<button>paypal</button>
-			<button>paypal</button>
+			<button className={styles.firstBtn}>paypal</button>
+			<button className={styles.secondBtn}>paypal</button>
 		</div>
 	)
 }
