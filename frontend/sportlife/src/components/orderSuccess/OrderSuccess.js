43ea0beb03c702ab6aa7f62277e80b83e0b5ff9a
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-22 12:05:26 -0300

    feat: Responsiveness concepts applied to the purchase thank you page

--- frontend/sportlife/src/components/orderSuccess/OrderSuccess.js
+++ frontend/sportlife/src/components/orderSuccess/OrderSuccess.js
@@ -8,22 +8,27 @@ import useUser from "../../hooks/useUser";
 
 import { Link } from "react-router-dom";
 
+import { FaRegCheckCircle } from 'react-icons/fa'
+
 export default function OrderSuccess() {
 
 	const { user } = useUser();
 
 	const [confetti, setConfetti] = useState(false)
 	useEffect(()=>{
-		//setConfetti(true)
+		setConfetti(true)
 	})
   return (
     <div className={styles.container}>
 			<div className={styles.subContainer}>
+				<div className={styles.checkCircle}>
+					<FaRegCheckCircle />
+				</div>
 				<div className={styles.infos}>
 					<h1>pagamento aprovado !</h1>
 					<img src='https://i.imgur.com/7WIlAHG.png' />
 					<p>{user.firstName} {user.lastName},</p>
-					<p>Obrigado por seu pedido na SportLife !</p>
+					<p>Obrigado por seu pedido na SportLife!</p>
 					<p>Assim que seu pacote for enviado, enviaremos um número de rastreamento.</p>
 					<Link to='/'>
 						<button>voltar as compras</button>
