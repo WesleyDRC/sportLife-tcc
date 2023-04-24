e91c4e5b26d06ce64c3798522556da242a4d7fd4
Wesley <88836392+WesleyDRC@users.noreply.github.com>
2023-04-24 14:09:37 -0300

    refactor: error handling when trying to create a purchase order

--- backend/src/modules/order/repositories/IOrderRepository.ts
+++ backend/src/modules/order/repositories/IOrderRepository.ts
@@ -3,7 +3,6 @@ import { OrderDetails } from "../infra/typeorm/entities/OrderDetails"
 import ICreateOrderDTO from "../dtos/ICreateOrderDTO"
 
 export interface IOrderRepository {
-	getCartUser(userId: string): Promise<any>
   createOrder(data: ICreateOrderDTO): Promise<OrderDetails>
-  findById(id: string): Promise<OrderDetails | undefined>;
+  // getOrder(): Promise<>
 }
