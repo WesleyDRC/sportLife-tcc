8820225eab23e2e3c5f2ae43c74f0c4110ad429a
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-27 14:40:14 -0300

    feat: adding upload logo on category page

--- frontend/sportlife/src/components/category/Category.js
+++ frontend/sportlife/src/components/category/Category.js
@@ -1,41 +1,43 @@
-import styles from './Category.module.css'
-import Card from './components/Card'
-import priceBRL from '../../utils/formatPrice'
+import styles from "./Category.module.css";
+import Card from "./components/Card";
+import priceBRL from "../../utils/formatPrice";
 
-import AxiosRepository from '../../repository/AxiosRepository'
+import AxiosRepository from "../../repository/AxiosRepository";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-export default function Category(props){
-	const [products, setProducts] = useState([])
-	useEffect(() => {
-    AxiosRepository.findAll({ order: 'views' })
-      .then(resp => {
-        setProducts(resp.data)
+import Loading from "../home/main/components/Loading";
+
+export default function Category(props) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    AxiosRepository.findAll({ order: "views" })
+      .then((resp) => {
+        setProducts(resp.data);
+        setLoading(false)
       })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [])
-	return(
-		<div className={styles.container}>
-			{/* <Card link='https://i.imgur.com/pnoGRSq.png' name='Camisa preta nike' price='100,00' />
-			<Card link='https://i.imgur.com/pnoGRSq.png' name='Camisa preta nike' price='100,00' />
-			<Card link='https://i.imgur.com/pnoGRSq.png' name='Camisa preta nike' price='100,00' />
-			<Card link='https://i.imgur.com/pnoGRSq.png' name='Camisa preta nike' price='100,00' /> */}
-			{products && products.length > 0 && (
-            products.map((item,i) => (
-              <Card
-                key={item.id}
-                id = {products[i].id}
-								src={products[i].imageMain}
-								name={products[i].name}
-                sexo={products[i].sexo}
-                category={products[i].categories.name}
-								price={priceBRL(products[i].price)}
-              />
-            ))
-          )}
-		</div>
-	)
+      .catch((error) => {
+        setLoading(false)
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className={styles.container}>
+      {products &&
+        products.length > 0 &&
+        products.map((item, i) => (
+          <Card
+            key={item.id}
+            id={products[i].id}
+            src={products[i].imageMain}
+            name={products[i].name}
+            sexo={products[i].sexo}
+            category={products[i].categories.name}
+            price={priceBRL(products[i].price)}
+          />
+        ))}
+      {loading && <Loading />}
+    </div>
+  );
 }
