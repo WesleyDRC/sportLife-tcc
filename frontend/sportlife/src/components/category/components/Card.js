d0696a3ebb5584f181fa0fdcd563c03dd3fe9e4e
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-27 14:29:11 -0300

    refactor : connecting category page with database

--- frontend/sportlife/src/components/category/components/Card.js
+++ frontend/sportlife/src/components/category/components/Card.js
@@ -1,28 +1,18 @@
 import styles from './Card.module.css'
-import {useState} from 'react'
+
+import {Link} from 'react-router-dom'
 
 export default function Card(props){
-
-	const [buttonView, setButtonView] = useState(false)
-
-	function buttonVisible(){
-		setButtonView(true)
-	}
-
-	function buttonNotVisible(){
-		setButtonView(false)
-	}
-
 	return(
 		<div  className={styles.container}>
-			<div onMouseOver={buttonVisible} onMouseOut={buttonNotVisible} className={styles.card}>
-				<img src={props.link} alt='Foto do produto' />
-				<p className={styles.name}>{props.name}</p>
-				<p className={styles.price}>R${props.price} </p>
-				<div className={styles.positionButton}>
-					<button className={buttonView ? styles.buy : styles.buyNone}>Adicionar ao carrinho</button>
+			<Link to={`/product/${props.id}`}>
+				<div className={styles.card}>
+					<img src={props.src} alt='Foto do produto' />
+					<p className={styles.name}>{props.name}</p>
+					<p className={styles.category}>{props.category}</p>
+					<p className={styles.price}>{props.price} </p>
 				</div>
-			</div>
+			</Link>
 		</div>
 	)
 }
