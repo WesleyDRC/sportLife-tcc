9f8fc5dcafe28302c3d2c1b60f327e109160beb6
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-28 16:31:27 -0300

    feat: starting order card

--- frontend/sportlife/src/components/profile/components/Request.js
+++ frontend/sportlife/src/components/profile/components/Request.js
@@ -1,10 +1,13 @@
 import styles from './Request.module.css'
 
+import CardOrder from './CardOrder'
+
 export default function Request(){
 	return(
 		<div className={styles.container}>
 			<h1 className={styles.title}>Pedidos</h1>
 			<div className={styles.subContainer}>
+				<CardOrder />
 			</div>
 		</div>
 	)
