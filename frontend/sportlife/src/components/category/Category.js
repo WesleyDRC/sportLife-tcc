c338899e8ba61052549e21ca447ed7b7d6b29c82
pablobidiaki <pablobidiaki@hotmail.com>
2023-04-29 17:14:13 -0300

    feat: adding what was searched and the amount of results when doing a search

--- frontend/sportlife/src/components/category/Category.js
+++ frontend/sportlife/src/components/category/Category.js
@@ -14,25 +14,28 @@ export default function Category(props) {
 
   return (
     <div className={styles.container}>
-      {props.products && props.products.length > 0 ? (
-        props.products.map((item, i) => (
-          <Card
-            key={item.id}
-            id={props.products[i].id}
-            src={props.products[i].imageMain}
-            name={props.products[i].name}
-            sexo={props.products[i].sexo}
-            category={props.products[i].categories.name}
-            price={priceBRL(props.products[i].price)}
-          />
-        ))
-      ) : (
-        <div className={styles.noResult}>
-          <p>Não foi encontrado nenhum resultado para "{`${name}`}".</p>
-          <p className={styles.otherProducts}>Tente buscar por outros produtos!</p>
-        </div>
-      )}
-      {props.loading && <Loading />}
+      <p className={props.products && props.products.length > 0 ? styles.on : styles.off}>{`${name}`} ({props.products.length})</p>
+      <div className={styles.subContainer}>
+        {props.products && props.products.length > 0 ? (
+          props.products.map((item, i) => (
+            <Card
+              key={item.id}
+              id={props.products[i].id}
+              src={props.products[i].imageMain}
+              name={props.products[i].name}
+              sexo={props.products[i].sexo}
+              category={props.products[i].categories.name}
+              price={priceBRL(props.products[i].price)}
+            />
+          ))
+        ) : (
+          <div className={styles.noResult}>
+            <p>Não foi encontrado nenhum resultado para "{`${name}`}".</p>
+            <p className={styles.otherProducts}>Tente buscar por outros produtos!</p>
+          </div>
+        )}
+        {props.loading && <Loading />}
+      </div>
     </div>
   );
 }
